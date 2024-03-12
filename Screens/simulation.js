ce045b4a8bdfb3c9261febdb14c3719a98608cd9
Preston Chapman <pchapman82070@uat.edu>
2024-03-12 12:45:59 -0700

    React Native Alert

--- Screens/simulation.js
+++ Screens/simulation.js
@@ -1,29 +1,24 @@
-import { Text, Pressable } from 'react-native';
+import { Text, Pressable, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { styles, values } from '../styles';
 import { header, footer } from '../default';
 
-
-const alertText = "Welcome to the simulation! However we appologize, but this page is not ready yet! Please come back soon and the simulation will be fully compleate!"
+const alertText =
+  'Welcome to the simulation! However we appologize, but this page is not ready yet! Please come back soon and the simulation will be fully complete!';
 
 // This is the function for the second scree, the navigation is unused as this page dose not load any others
 function SecondScreen({ navigation }) {
-
   return (
     <LinearGradient
       style={[styles.information, styles.content]}
-      colors={[
-        values.bg.rawUmber.line(),
-        values.bg.eerieBlack.line(),
-      ]}>
+      colors={[values.bg.rawUmber.line(), values.bg.eerieBlack.line()]}>
       {header()}
       <Pressable
-          style={styles.border}
-          onPress={() => alert(alertText)}>
-          <Text style={[styles.text, { color: 'rgb(0,255,0)' }]}>
-                    Launch!
-          </Text>
+        style={styles.border}
+        // alert(alertText) can be used, however importing alert from react-native has more options
+        onPress={() => Alert.alert('Not Available', alertText)}>
+        <Text style={[styles.text, { color: 'rgb(0,255,0)' }]}>Launch!</Text>
       </Pressable>
       {footer()}
     </LinearGradient>
