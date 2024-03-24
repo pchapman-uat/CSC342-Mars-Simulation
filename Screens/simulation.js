f94556ca9ea78c60ac55e65c53890d105275703c
Preston Chapman <pchapman82070@uat.edu>
2024-03-24 10:21:45 -0700

    Drop Drown, and sound effects [Simulation]

--- Screens/simulation.js
+++ Screens/simulation.js
@@ -1,44 +1,129 @@
-import { Text, Pressable, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Text, Pressable, View, TextInput, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-import { styles, values } from '../styles';
-import { footer } from '../default';
-import { TextInput } from 'react-native-gesture-handler';
+import SelectDropdown from "react-native-select-dropdown"
+
+import { styles, values, RGBA } from '../styles';
+import { footer, playAudio } from '../default';
+
+
 
 const alertText =
-  'Welcome to the simulation! However we appologize, but this page is not ready yet! Please come back soon and the simulation will be fully complete!';
+  'Welcome to the simulation! However we apologize, but this page is not ready yet! Please come back soon and the simulation will be fully complete!';
 
-// This is the function for the second screen, the navigation is unused as this page dose not load any others
+
+class User{
+  constructor(firstName, lastName, age){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+  welcome() {
+    return `Welcome ${this.firstName} ${this.lastName}! (age: ${this.age}) The simulation will begin soon!`
+  }
+}
+
 function SecondScreen({ navigation }) {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName ] = useState('')
+  
+  const [lastNameColor, setLastNameColor ] = useState(values.borderBg.line())
+  const [firstNameColor, setFirstNameColor ] = useState(values.borderBg.line())
+  const [userAge, setUserAge ] = useState()
+  const [ ageColor, setAgeColor] = useState(values.borderBg.line())
+    const ages = [];
+
+    for (let i = 13; i <= 100; i++) {
+      ages.push(i);
+    }
+
+  const handleSubmit = async () => {
+      updateFirstName(firstName)
+      updateLastName(lastName)
+      updateAge(userAge)
+      if(firstName.length > 0 && firstName.length > 0 && userAge) {
+        await playAudio(0.02, require('../assets/audio/correct.mp3'))
+        let user = new User(firstName, lastName, userAge)
+        alert(user.welcome())
+      } else playAudio(0.02, require('../assets/audio/wrong.mp3'))
+    };
+
+  const updateFirstName = (firstName) => {
+      setFirstName(firstName)
+      if (firstName.length === 0) {
+        setFirstNameColor(simValues.inputError.line());
+      } else {
+        setFirstNameColor(values.borderBg.line())
+      }
+  }
+
+  const updateLastName = (lastName) => {
+      setLastName(lastName)
+      if (lastName.length === 0) {
+        setLastNameColor(simValues.inputError.line());
+      } else {
+        setLastNameColor(values.borderBg.line())
+      }
+  }
+    const updateAge = (age) => {
+      setUserAge(age)
+      if (!age) {
+        setAgeColor(simValues.inputError.line());
+      } else {
+        setAgeColor(values.borderBg.line())
+      }
+  }
+
   return (
     <LinearGradient
       style={[styles.information, styles.content]}
       colors={[values.bg.rawUmber.line(), values.bg.eerieBlack.line()]}>
+      <form>
       <View style={simStyle.inputBoxContainer}>
         <View>
           <Text style={simStyle.inputLable}>First Name</Text>
-          <TextInput style={simStyle.input}></TextInput>
+          <TextInput
+            style={[simStyle.input, {backgroundColor: firstNameColor}]}
+            value={firstName}
+            onChangeText={(text) => updateFirstName(text)}
+          />
         </View>
         <View>
           <Text style={simStyle.inputLable}>Last Name</Text>
-          <TextInput style={simStyle.input}></TextInput>
+          <TextInput 
+            style={[simStyle.input, {backgroundColor: lastNameColor}]}
+            value={lastName}
+            onChangeText={(text) => updateLastName(text)}></TextInput>
         </View>
       </View>
-
-      {/* TODO: Pressable does not do anything */}
-      {/* TODO: Impliment Alert */}
-      <Pressable style={simStyle.submitButton}>
-        <Text style={[styles.text, {color: values.submit.line()}]}>Submit!</Text>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <SelectDropdown 
+          buttonStyle={[styles.border, {height: 30, backgroundColor: ageColor}]} 
+          buttonTextStyle={styles.text} 
+          data={ages}
+          defaultButtonText="Age"
+          value={userAge}
+          onSelect={(value) => updateAge(value)}/>
+        <Text style={simStyle.dropDownText}>▼</Text>
+      </View>
+      </form>
+      
+      <Pressable style={simStyle.submitButton} onPress={handleSubmit}>
+        <Text style={[styles.text, { color: values.submit.line() }]}>Submit!</Text>
       </Pressable>
+
       {footer()}
     </LinearGradient>
   );
 }
-
 // This exports the Second Screen to be used in the App.js
 export default SecondScreen;
 
-// A second style sheet is created for this page specificly
+const simValues = {
+    inputError: new RGBA(255,0,0, 0.3)
+}
+
 const simStyle = new StyleSheet.create({
   input: [
       styles.border,
@@ -56,9 +141,14 @@ const simStyle = new StyleSheet.create({
   inputLable: {
     textAlign:'center',
     color: values.fontColor.line(),
-    paddingBottom: 10
+    paddingBottom: 5,
   },
-
+  dropDownText: {
+    position: 'absolute', 
+    marginLeft: "45%", 
+    marginBottom: 15, 
+    color: values.fontColor.line()
+    },
   submitButton: [
     styles.border,
     {
