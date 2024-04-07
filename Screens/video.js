dbd36dd7480503a81e40098816c82ec9ae6b384f
Preston Chapman <pchapman82070@uat.edu>
2024-04-07 08:13:42 -0700

    Video Screen Formating

--- Screens/video.js
+++ Screens/video.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
+import { styles, values } from '../styles';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({ isPlaying: 'Pause'});
   return (
-    <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={require('../assets/video/SolarSystem_NationalGeographic.mp4')}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-          }
+    <LinearGradient style={videoStyles.content}
+    colors={[values.bg.champagne.line(), values.bg.rawUmber.line()]}>
+      <View>
+        <Video
+          ref={video}
+          style={videoStyles.video}
+          source={require('../assets/video/SolarSystem_NationalGeographic.mp4')}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
         />
+        <View style={videoStyles.buttons}>
+          <Button
+            title={status.isPlaying ? 'Pause' : 'Play'}
+            onPress={() =>
+              status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+            }
+          />
+        </View>
       </View>
-    </View>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-  },
+const videoStyles = StyleSheet.create({
+  content: [
+    styles.content,
+    {
+      backgroundColor: values.bg.rawUmber.line(),
+    }
+  ],
   video: {
     alignSelf: 'center',
     width: 320,
