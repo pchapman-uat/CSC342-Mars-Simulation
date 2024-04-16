5866d111ce0ef724dd842ad82dd2ab75ee86e08e
Preston Chapman <pchapman82070@uat.edu>
2024-04-16 13:15:41 -0700

    Planet styles in separate stylesheet

--- Screens/simulation.js
+++ Screens/simulation.js
@@ -1,110 +1,135 @@
 import { Text, ScrollView, ImageBackground, StyleSheet, View} from 'react-native'
 import { RGB, RGBA, styles, values } from '../styles'
 function SimulationScreen({navigation}){
-    const simStyles = new StyleSheet.create({
-        mercury: {
+
+    const simStyle = StyleSheet.create({
+        planet: {
+            backgroundColor: new RGBA(255,255,255,0.5).line(),
+        },
+    })
+    const planetStyles = new StyleSheet.create({
+    // Idealy this would be within the same stylesheet as the simStyle but this was not possible as of now
+        mercury: [
+            simStyle.planet,
+             {
             height: 50,
             width: 50,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "18.1%",
             top: "50.5%"
-        },
-        venus: {
+             }
+            ],
+        venus: [
+            simStyle.planet,
+             {
             height: 180,
             width: 180,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "20.8%",
             top: "39%"
-        },
-        earth: {
+             }
+            ],
+        earth: [
+            simStyle.planet,
+             {
             height: 180,
             width: 180,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "30%",
             top: "39%"
-        },
-        mars: {
+             }
+            ],
+        mars: [
+            simStyle.planet,
+             {
             height: 80,
             width: 80,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "39.5%",
             top: "48%"
-        },
-        jupiter: {
+             }
+            ],
+        jupiter: [
+            simStyle.planet,
+             {
             height: 250,
             width: 250,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "43.7%",
             top: "34.5%"
-        },
-        saturn: {
+             }
+            ],
+        saturn: [
+            simStyle.planet,
+             {
             height: 260,
             width: 375,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "56.8%",
             top: "30%"
-        },
-        uranus: {
+             }
+            ],
+        uranus: [
+            simStyle.planet,
+             {
             height: 170,
             width: 170,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "75.8%",
             top: "33.5%"
-        },
-        neptune: {
+             }
+            ],
+        neptune: [
+            simStyle.planet,
+             {
             height: 170,
             width: 170,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "85.1%",
             top: "33.5%"
-        },
-        pluto: {
+             }
+            ],
+        pluto: [
+            simStyle.planet,
+             {
             height: 50,
             width: 50,
-            backgroundColor: new RGBA(255,255,255,0.5).line(),
             position: 'absolute',
             left: "94.5%",
             top: "43%"
-        },
+             }
+            ],
         
     })
     return(
         <ScrollView horizontal={true} style={{backgroundColor: new RGB(0,0,0).line()}}>
             <ImageBackground style={{width:2000}} source={require("../assets/images/test.png")}>
-            <View style={simStyles.mercury}>
+            <View style={planetStyles.mercury}>
 
             
             </View>
-            <View style={simStyles.venus}>
+            <View style={planetStyles.venus}>
 
             </View>
-            <View style={simStyles.earth}>
+            <View style={planetStyles.earth}>
 
             </View>
-            <View style={simStyles.mars}>
+            <View style={planetStyles.mars}>
 
             </View>
-            <View style={simStyles.jupiter}>
+            <View style={planetStyles.jupiter}>
 
             </View>
-            <View style={simStyles.saturn}>
+            <View style={planetStyles.saturn}>
 
             </View>
-            <View style={simStyles.uranus}>
+            <View style={planetStyles.uranus}>
 
             </View>
-            <View style={simStyles.neptune}>
+            <View style={planetStyles.neptune}>
 
             </View>
-            <View style={simStyles.pluto}>
+            <View style={planetStyles.pluto}>
 
             </View>
             
