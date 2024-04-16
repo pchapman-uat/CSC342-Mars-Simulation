77702a4c4f347d1cdcc8013260f528f31b12dcfe
Preston Chapman <pchapman82070@uat.edu>
2024-04-16 12:35:09 -0700

    Add Space for mars button

--- Screens/simulation.js
+++ Screens/simulation.js
@@ -1,13 +1,24 @@
-import { Text, ScrollView, ImageBackground, StyleSheet} from 'react-native'
-import { RGB, styles, values } from '../styles'
+import { Text, ScrollView, ImageBackground, StyleSheet, View} from 'react-native'
+import { RGB, RGBA, styles, values } from '../styles'
 function SimulationScreen({navigation}){
+    const simStyles = new StyleSheet.create({
+        testBtn: {
+            height: 100,
+            width: 100,
+            backgroundColor: new RGBA(0,0,0,0.5).line()
+        }
+    })
     return(
         <ScrollView horizontal={true} style={{backgroundColor: new RGB(0,0,0).line()}}>
             <ImageBackground style={{width:2000}} source={require("../assets/images/test.png")}>
+                <View style={simStyles.testBtn}>
+
+                </View>
             
             </ImageBackground>
         </ScrollView>
     )
 
 }
+
 export default SimulationScreen
