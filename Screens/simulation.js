70607309fa383159432e641df6b33c0db2c1c637
Preston Chapman <pchapman82070@uat.edu>
2024-04-16 13:59:36 -0700

    Display info and change color

--- Screens/simulation.js
+++ Screens/simulation.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
     Text,
     ScrollView,
@@ -9,6 +10,8 @@ import {
   } from 'react-native';
   import { RGB, RGBA, styles, values } from '../styles';
   function SimulationScreen({ navigation }) {
+    const [infoColor, setInfoColor] = useState(new RGBA(0,0,0,0))
+    const [info, setInfo ] = useState("")
     // Idealy this would be within the same stylesheet as the planets but this was not possible
     const simStyle = StyleSheet.create({
       planet: {
@@ -20,7 +23,7 @@ import {
         top: 0,
         left: 0,
         right: 0,
-        backgroundColor: new RGBA(255,255,255,0.7).line(),
+        backgroundColor: infoColor.line(),
         margin: 10,
         borderRadius: "20px",
         justifyContent: 'center',
@@ -128,37 +131,54 @@ import {
     });
     const planets = {
         mercury: {
-          description: "The smallest planet, closest to the Sun. It has a rocky surface and extreme temperature variations."
+            description: "The smallest planet, closest to the Sun. It has a rocky surface and extreme temperature variations.",
+            color: new RGB(194, 194, 194)
         },
         venus: {
-          description: "Similar in size to Earth, it's known for its thick, toxic atmosphere and intense greenhouse effect, making it the hottest planet in our solar system."
+            description: "Similar in size to Earth, it's known for its thick, toxic atmosphere and intense greenhouse effect, making it the hottest planet in our solar system.",
+            color: new RGB(239, 226, 105)
         },
         earth: {
-          description: "Our home planet, with a diverse environment that supports life as we know it. It has a unique combination of land, water, and atmosphere."
+            description: "Our home planet, with a diverse environment that supports life as we know it. It has a unique combination of land, water, and atmosphere.",
+            color: new RGB(91, 155, 213)
         },
         mars: {
-          description: "Often called the 'Red Planet' due to its rusty color. It has polar ice caps, canyons, and extinct volcanoes, and it's a focus of exploration for its potential to harbor life."
+          description: "Often called the 'Red Planet' due to its rusty color. It has polar ice caps, canyons, and extinct volcanoes, and it's a focus of exploration for its potential to harbor life.",
+          color: new RGB(228, 96, 96)
         },
         jupiter: {
-          description: "The largest planet, known for its massive size and iconic swirling bands of gas. It has a dynamic atmosphere and a large number of moons, including the four Galilean moons."
+            description: "The largest planet, known for its massive size and iconic swirling bands of gas. It has a dynamic atmosphere and a large number of moons, including the four Galilean moons.",
+            color: new RGB(244, 187, 99)
         },
         saturn: {
-          description: "Known for its spectacular ring system, Saturn is the second-largest planet. It's mostly composed of hydrogen and helium gas, with a rocky core."
+            description: "Known for its spectacular ring system, Saturn is the second-largest planet. It's mostly composed of hydrogen and helium gas, with a rocky core.",
+            color: new RGB(218, 214, 203)
         },
         uranus: {
-          description: "An ice giant with a unique feature: it rotates on its side. Uranus has a faint ring system and a blue-green color due to the presence of methane in its atmosphere."
+            description: "An ice giant with a unique feature: it rotates on its side. Uranus has a faint ring system and a blue-green color due to the presence of methane in its atmosphere.",
+            color: new RGB(133, 204, 239)
         },
         neptune: {
-          description: "Similar in composition to Uranus, Neptune is the farthest planet from the Sun. It has strong winds and a striking blue coloration, with faint rings and a large moon, Triton."
+            description: "Similar in composition to Uranus, Neptune is the farthest planet from the Sun. It has strong winds and a striking blue coloration, with faint rings and a large moon, Triton.",
+            color: new RGB(68, 109, 162)
         },
         pluto: {
-          description: "Classified as a dwarf planet, Pluto is located in the Kuiper Belt. It's composed of ice and rock and has a thin atmosphere. Pluto has five known moons, with its largest moon, Charon, being particularly notable for its size relative to Pluto."
+            description: "Classified as a dwarf planet, Pluto is located in the Kuiper Belt. It's composed of ice and rock and has a thin atmosphere. Pluto has five known moons, with its largest moon, Charon, being particularly notable for its size relative to Pluto.",
+            color: RGB(180, 169, 169)
         }
       };
 
     function handlePressinfo(key){
-        console.log(planets[key].description)
+        console.log(`Diplaying ${key}`)
+        let currentInfo = planets[key]
+        setInfoColor(currentInfo.color)
+        setInfo(currentInfo.description)
     }
+
+
+
+    
+
     return (
         <View style={simStyle.container}>
             <ScrollView
@@ -180,7 +200,7 @@ import {
             </ScrollView>
             <View style={simStyle.overlay}>
                 <Text style={simStyle.infoText}>
-                    {planets.mars.description}
+                    {info}
                 </Text>
             </View>
         </View>
